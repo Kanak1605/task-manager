1d3fdf35791beb03f599212a6af9fab3efe4f2fd
Kanak Chaurasia <kanakchaurasia724@gmail.com>
2026-05-01 03:38:00 +0530

    Initial commit

--- server/index.js
+++ server/index.js
@@ -10,8 +10,10 @@ app.get("/", (req, res) => {
   res.send("Backend running 🚀");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 import authRoutes from "./routes/auth.js";
 
