f356ec9f7c65379193ae428051b530cd4d109bd4
Kanak Chaurasia <kanakchaurasia724@gmail.com>
2026-05-01 12:20:30 +0530

    test

--- server/index.js
+++ server/index.js
@@ -1,30 +1,17 @@
 import express from "express";
 import cors from "cors";
 
-import authRoutes from "./routes/auth.js";
-import taskRoutes from "./routes/task.js";
-import projectRoutes from "./routes/project.js";
-
 const app = express();
 
-// middlewares
 app.use(cors());
 app.use(express.json());
 
-// test route
 app.get("/", (req, res) => {
-  res.send("Backend running 🚀");
+  res.send("Server working ✅");
 });
 
-// routes
-app.use("/auth", authRoutes);
-app.use("/tasks", taskRoutes);
-app.use("/projects", projectRoutes);
-
-// port
 const PORT = process.env.PORT || 5000;
 
-// start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
