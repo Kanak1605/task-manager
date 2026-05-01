cdb0666eb01cb58d7f12e9699dec82cac7d1870c
Kanak Chaurasia <kanakchaurasia724@gmail.com>
2026-05-01 11:48:54 +0530

    fix 502 issue

--- server/index.js
+++ server/index.js
@@ -1,26 +1,30 @@
 import express from "express";
 import cors from "cors";
 
+import authRoutes from "./routes/auth.js";
+import taskRoutes from "./routes/task.js";
+import projectRoutes from "./routes/project.js";
+
 const app = express();
 
+// middlewares
 app.use(cors());
 app.use(express.json());
 
+// test route
 app.get("/", (req, res) => {
   res.send("Backend running 🚀");
 });
 
+// routes
+app.use("/auth", authRoutes);
+app.use("/tasks", taskRoutes);
+app.use("/projects", projectRoutes);
+
+// port
 const PORT = process.env.PORT || 5000;
 
+// start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-import authRoutes from "./routes/auth.js";
-
-app.use("/auth", authRoutes);
-import taskRoutes from "./routes/task.js";
-
-app.use("/tasks", taskRoutes);
-import projectRoutes from "./routes/project.js";
-
-app.use("/projects", projectRoutes);
